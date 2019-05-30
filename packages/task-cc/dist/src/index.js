8ede818a061058e064131563c410279026775b20
Štefan Töltési <stefan.toltesi@gmail.com>
2019-05-30 13:33:57 +0200

    cleaning repo

--- packages/task-cc/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./task.model"), exports);
-tslib_1.__exportStar(require("./task.controller"), exports);
-//# sourceMappingURL=index.js.map
